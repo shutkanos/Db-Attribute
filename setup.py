a856eae7975988555182d284936fbb8da231a7c0
Shutkanos <k4r8mgy1kgkf@mail.ru>
2025-08-22 16:29:19 +0300

    Update v2.1.1

--- setup.py
+++ setup.py
@@ -25,9 +25,7 @@ setup(
     license='MIT',
 
     classifiers=[
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
